59213513f287e980b3f2ec42d7ce971d22a5a1ff
Kevin Taylor <kevintay96@gmail.com>
2022-07-27 14:49:57 -0600

    created seed and get create and update key functions

---
+++ Server/controller.js
@@ -0,0 +1,70 @@
+require(`dotenv`).config()
+const Sequelize = require(`sequelize`)
+
+const { CONNECTION_STRING } = process.env
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+module.exports = 
+{
+    seed: (req,res) =>
+    {
+        sequelize.query
+        (`
+             CREATE TABLE keys (
+                   key_id SERIAL PRIMARY KEY,
+                   key_name varchar(255) NOT NULL,
+                   use_descr text,
+                   use_location varchar(255)
+                   );
+                
+                 INSERT INTO keys(key_name, use_descr, use_location)
+                 VALUES ('factory exit', 'Med tent and underground exits in factory. ZB-013 and checkpoint doors between new gas and boilers on customs', 'Customs and Factory'),
+                 ('Emercom Medical Unit key', 'Opens medical area in front of Kostin', 'Interchange'),
+                 ('RB-ORB1', 'Opens armory door east end 4th floor white pawn building. Needed for Inventory Check quest', 'Reserve'),
+                 ('ZB-014', 'Opens door in bunker near scav house. Can have 5.45 60 round mags', 'Woods');
+            `)
+    },
+
+    getkeys: (req, res) =>
+    {
+        sequelize.query
+        (
+            `
+            SELECT * FROM keys;
+            `
+        )
+        .then(dbRes => res.status(200).send(dbRes[0]))
+    },
+
+    createKey: (req, res) =>
+    {
+        const { name, useDescr, useLocation } = req.body;
+
+        sequelize.query
+        (`
+            INSERT INTO keys (name, use_descr, use_location)
+            VALUES ('${name}', '${useDescr}', '${useLocation}')
+        `)
+    },
+    
+    updateKey: (req, res) =>
+    {
+        const { id, name, useDescr, useLocation} = req.params;
+
+        sequelize.query
+        (`
+            UPDATE keys
+            SET name = '${name}', use_descr = '${useDescr}', use_location = '${useLocation}'
+            WHERE key_id = ${id};
+        `)
+    },
+
+}
