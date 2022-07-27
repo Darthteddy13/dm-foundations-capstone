24d4006b1ba17d059b1bd612bb51c795133f60e6
Kevin Taylor <kevintay96@gmail.com>
2022-07-27 15:19:32 -0600

    added middleware for css and js files

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,20 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env
+
+const port = process.env.PORT || 5050;
+
 const { getKeys, createKey, updateKey, deleteKey } = require('./controller.js')
+
+
+app.get(`/`, (req, res) =>
+{
+    res.sendFile(path.join(__dirname, `../index.html`))
+})
+
+app.get(`/css`, (req, res) => res.sendFile(path.join(__dirname, `../style.css`)))
+
+app.get(`/js`, (req, res) => res.sendFile(path.join(__dirname, `../index.js`)))
 
 app.use(express.json())
 app.use(cors())
@@ -15,4 +26,7 @@ app.post(`/keys`, createKey);
 app.put(`/keys/:id`, updateKey);
 app.delete(`/keys/:id`, deleteKey);
 
-app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
+
+
+
+app.listen(port, () => console.log(`up on ${port}`));
