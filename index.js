031b2d553cbb497afb10c220a3033920fbb14ece
Kevin Taylor <kevintay96@gmail.com>
2022-08-02 11:55:48 -0600

    removed .env and updated some css for body

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const getKeys = () =>
             console.table(res.data)
             res.data.forEach(e => 
                 {
-                    
                     let keyCard = `<div class="key-card">
-                    <h2> ${e.key_name}, ${e.use_location}</h2>
+                    <h2> ${e.key_name}</h2>
+                    <h2>${e.use_location}</h2>
                     <h3> ${e.use_descr} </h3>
                     <button onclick="deleteCard(${e['key_id']})">Delete</button>
                     </div>
